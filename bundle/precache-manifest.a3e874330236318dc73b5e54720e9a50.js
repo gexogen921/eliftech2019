e1ba1c1da7f65f1f8e473ad878124cf8193afc72
gexogen921 <thegexogen2012@gmail.com>
2019-08-18 21:46:48 +0300

    Updated the endpointds regarding heroku

---
+++ bundle/precache-manifest.a3e874330236318dc73b5e54720e9a50.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52daff889d349171493ba8d5f91a902e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a0eeee5101ab97b802f1",
+    "url": "/static/css/main.37684c2d.chunk.css"
+  },
+  {
+    "revision": "b94f5b173f3d1b139cd0",
+    "url": "/static/js/2.81a37b69.chunk.js"
+  },
+  {
+    "revision": "a0eeee5101ab97b802f1",
+    "url": "/static/js/main.9a62e616.chunk.js"
+  },
+  {
+    "revision": "0eee51fb73153424a7d5",
+    "url": "/static/js/runtime~main.2c0f3e42.js"
+  }
+]);
